d945b1e7bbf7832db7840e7589f6114894cea5ea
Orcha02 <orcha2002@gmail.com>
2021-09-13 18:22:40 -0500

    Addtodo list progress script and readme

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Given employee ID, returns information about his/her TODO list progress"""
+import requests
+from sys import argv
+import json
+
+if __name__ == "__main__":
+    user_id = argv[1]
+
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = "{}users/{}".format(url, user_id)
+    employee_dict = requests.get(user).json()
+    employee_name = employee_dict.get("name")
+
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos',
+                         params={'userId': user_id})
+    todo = todo.json()
+    total_todo = len(todo)
+
+    todo_done = [task for task in todo if task['completed'] is True]
+    total_todo_done = len(todo_done)
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, total_todo_done, total_todo))
+    for info_dict in todo_done:
+        task_title = info_dict.get("title")
+        print("\t " + task_title)
---
+++ 0x15-api/README.md
@@ -0,0 +1,6 @@
+| FILES  | DESCRIPTION |
+| ------------- | ------------- |
+| 0-gather_data_from_an_API.py | Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress |
+| 1-export_to_CSV.py | Based on '0-gather_data_from_an_API.py', extend your Python script to export data in the CSV format. |
+| 2-export_to_JSON.py | Based on '0-gather_data_from_an_API.py', extend your Python script to export data in the JSON format. |
+| 3-dictionary_of_list_of_dictionaries.py | Based on '0-gather_data_from_an_API.py', extend your Python script to export data in the JSON format. |
